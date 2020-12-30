e5e94f5cd39cd9b80bb39e1f5eec6695a4e6ee4c
Ngoc Van Duong <van-ngoc.duong@ekino.com>
2020-12-30 14:00:12 +0700

    add project products

---
+++ products-app/src/components/ColorBox.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ChangeColorButton from './ChangeColorButton.js';
+
+class ColorBox extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            backgroundColor : "red",
+            width : '200px',
+            height : '200px'
+        }
+        this.changeColor = this.changeColor.bind(this);
+    }
+    changeColor () {
+        this.setState((prevState) => {
+            let newColor = prevState.backgroundColor === 'red' ? 'blue' : 'red';
+            return ({
+                backgroundColor: newColor
+            });
+        })
+    }
+    render() {
+        return (
+            <div style={{width: this.state.width, height:this.state.height, backgroundColor:this.state.backgroundColor}}>
+                <ChangeColorButton onClick={this.changeColor}/>
+            </div>
+        )
+    }
+}
+
+export default ColorBox;
